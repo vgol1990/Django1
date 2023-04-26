52baaaef8ffc7731e51ec1e6928dc40ff58a1095
vgol1990 <35070893+vgol1990@users.noreply.github.com>
2023-04-26 14:26:43 +0300

    lesson3

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for coolsite project.
+"""coolsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+<<<<<<< Updated upstream
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+=======
+
+from women.views import *
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('women/', include('women.urls')),
+>>>>>>> Stashed changes
 ]
