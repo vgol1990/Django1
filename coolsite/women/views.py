9b7d8574f17805d39a67f9c1a70c1034e078c35c
vgol1990 <35070893+vgol1990@users.noreply.github.com>
2023-05-02 14:10:15 +0300

    lesson6finish

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from  .models import *
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 def index(request): #HttpRequest
-    return HttpResponse("Страница приложения women.")
-
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница '})
+def about(request): #HttpRequest
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 def categories(request, catid):
     if request.POST:
         print(request.POST)
