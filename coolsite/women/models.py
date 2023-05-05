79c2cb75da53d40a70b440314bba30045832c27d
vgol1990 <35070893+vgol1990@users.noreply.github.com>
2023-05-05 13:32:06 +0300

    lesson10start

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
@@ -9,7 +8,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete= models.PROTECT, null= True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -17,8 +16,9 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index= True)
+    name = models.CharField(max_length=100, db_index=True)
 
     def __str__(self):
         return self.name
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,44 +1,55 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
-from  .models import *
 
-menu = [
-    {'title': 'О сайте', 'url_name' : 'about'},
-    {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name' : 'login'},
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
 ]
 
-def index(request): #HttpRequest
+
+def index(request):
     posts = Women.objects.all()
     cats = Category.objects.all()
 
-    context  = {
+    context = {
         'posts': posts,
-        'cats' : cats,
+        'cats': cats,
         'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0,
     }
-    return render(request, 'women/index.html', context= context)
-def about(request): #HttpRequest
+
+    return render(request, 'women/index.html', context=context)
+
+def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def addpage(request):
     return HttpResponse("Добавление статьи")
+
 def contact(request):
     return HttpResponse("Обратная связь")
+
 def login(request):
     return HttpResponse("Авторизация")
+
+
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id= {post_id}")
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
     cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
 
     context = {
         'posts': posts,
@@ -47,4 +58,6 @@ def show_category(request, cat_id):
         'title': 'Отображение по рубрикам',
         'cat_selected': cat_id,
     }
-    return render(request, 'women/index.html', context = context)
+
+    return render(request, 'women/index.html', context=context)
+
